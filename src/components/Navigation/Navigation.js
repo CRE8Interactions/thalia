ab6dda5a218366347af1f0e332df5778087328d2
Jaime Convery <jaimeconvery@gmail.com>
2023-05-29 13:49:43 -0400

    fix: logo sizing

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,13 +6,20 @@ import { useRouter } from 'next/navigation';
 import Link from 'next/link'
 import Image from 'next/image';
 
+import { useWindowSize } from '@/utilities/hooks';
+
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
-import logo from '/public/images/logo.png';
+import desktopLogo from '/public/images/logo.png';
+import mobileLogo from '/public/images/logo-sm.png';
 
 export default function Navigation() {
+
+    const windowSize = useWindowSize();
+
+    const logo = windowSize < 768 ? mobileLogo : desktopLogo;
 
     const router = useRouter();
 
@@ -20,7 +27,7 @@ export default function Navigation() {
         <Navbar collapseOnSelect expand="md" className='navigation'>
             <Container>
                 <Link href="/" className="navbar-brand">
-                    <Image src={logo} alt="Southport Hall" width="67" height="92" />
+                    <Image src={logo} alt="Southport Hall" width="67" height="92" className='logo' />
                 </Link>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" id="navbar-toggle" />
                 <Navbar.Collapse id="responsive-navbar-nav">
