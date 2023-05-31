eb5465d9f09ed359ad117387104f284b86fc14d8
Jaime Convery <jaimeconvery@gmail.com>
2023-05-31 09:25:33 -0400

    refactor: add pdf links

--- src/components/PrivateEventsWrapper/Packages/File/File.js
+++ src/components/PrivateEventsWrapper/Packages/File/File.js
@@ -6,11 +6,11 @@ import pdf from '/public/images/icons/pdf.svg';
 
 import styles from './File.module.scss'
 
-export default function File({ title }) {
+export default function File({ title, link }) {
 
     return (
         <li className={styles.container}>
-            <Link href="" className="text-decoration-none">
+            <Link href={link} className="text-decoration-none" target="_blank">
                 <Image src={pdf} alt="" width="64" height="64" className='m-auto' />
                 <p className={styles.title}>{title}</p>
             </Link>
--- src/components/PrivateEventsWrapper/Packages/Packages.js
+++ src/components/PrivateEventsWrapper/Packages/Packages.js
@@ -12,10 +12,10 @@ export default function Packages() {
             <Card body className='card--light card-sm'>
                 <Card.Title as="h5" className='text-center'>Download our private event packages to learn more.</Card.Title>
                 <Stack as="ul" direction="horizontal" gap={3} className='flex-wrap justify-content-center gap-lg-4'>
-                    <File title="Private events" />
-                    <File title="Weddings" />
-                    <File title="Reunions" />
-                    <File title="Sweet 16 / Prom" />
+                    <File title="Private events" link="https://southport.nyc3.cdn.digitaloceanspaces.com/privateparty.pdf" />
+                    <File title="Weddings" link="https://southport.nyc3.cdn.digitaloceanspaces.com/weddingpackage.pdf" />
+                    <File title="Reunions" link="https://southport.nyc3.cdn.digitaloceanspaces.com/reunions.pdf" />
+                    <File title="Sweet 16 / Prom" link="https://southport.nyc3.cdn.digitaloceanspaces.com/sweetsixteen.pdf" />
                 </Stack>
             </Card>
         </section>
