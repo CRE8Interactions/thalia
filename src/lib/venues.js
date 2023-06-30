484a31a6123b1efb593981a5c5e9a5c98f1ffaa4
Chaz Haskins <chazhaskins@Chazs-MacBook-Pro.local>
2023-06-30 08:34:38 -0400

    revalidates cache after 30 sec

--- src/lib/venues.js
+++ src/lib/venues.js
@@ -1,6 +1,6 @@
 export async function getVenue(slug) {
     // Call an external API endpoint to get venues.
-    const res = await fetch(`${process.env.API}/venues/${slug}/events`)
+    const res = await fetch(`${process.env.API}/venues/${slug}/events`, { next: { revalidate: 30 } })
     const data = await res.json()
     return data
 }
