ea99b636e4cbde8b7249a77eebce444d489ef442
Chaz Haskins <chazhaskins@Chazs-MacBook-Pro.local>
2023-09-23 09:17:47 -0400

    Enables pixel

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import { Inter, DM_Sans } from 'next/font/google'
 
 import { Navigation, Footer } from '@/components'
+import Script from 'next/script';
 
 import '@/scss/main.scss';
 
@@ -49,6 +50,24 @@ export default function RootLayout({ children }) {
                     {children}
                 </main>
                 <Footer />
+                <Script
+                    id="fb-pixel"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                    __html: `
+                    !function(f,b,e,v,n,t,s)
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window, document,'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                    fbq('init', '842729400339292');
+                    fbq('track', 'PageView');
+                    `,
+                    }}
+                />
             </body>
         </html>
     )
