e17297dd1c944012af2124b79a996c8caeb8cc11
CRE8 Interactions <cre8interactions@gmail.com>
2023-10-19 11:36:14 -0400

    Revert "Fix issues with timezones"

--- src/utilities/helpers.js
+++ src/utilities/helpers.js
@@ -11,9 +11,9 @@ export const formatCurrency = (num) => {
 export const formatDateTime = (date, formatter) => {
     switch (formatter) {
         case 'timeOnly':
-            return date?.tz("America/Chicago").format('h:mma')
+            return date?.format('h:mma')
 
         default:
-            return date?.tz("America/Chicago").format('dddd, MMM D | h:mma')
+            return date?.format('dddd, MMM D | h:mma')
     }
 }
