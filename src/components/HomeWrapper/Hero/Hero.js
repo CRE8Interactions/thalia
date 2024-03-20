c2f359c3af5e9daede6284db0a749834c00bb273
Jaime Convery <jaimeconvery@gmail.com>
2024-03-20 14:30:21 -0400

    style: spacing

--- src/components/HomeWrapper/Hero/Hero.js
+++ src/components/HomeWrapper/Hero/Hero.js
@@ -9,8 +9,8 @@ import styles from './Hero.module.scss'
 
 export default function Hero() {
     return (
-        <Stack as="header" gap={2} className={`gap-md-1 text-center align-items-center ${styles.hero}`}>
-            <h1 className='heading-lg display-1 m-0'>Discover the latest <span className='d-flex gap-1'><Badge bg="default" className='badge-primary badge-lg badge--schedule'>upcoming</Badge> events</span></h1>
+        <Stack as="header" gap={2} className={`text-center align-items-center ${styles.hero}`}>
+            <h1 className='heading-lg display-1'>Discover the latest <span className='d-flex align-items-center gap-1'><Badge bg="default" className='badge-primary badge-lg badge--schedule'>upcoming</Badge> events</span></h1>
             <h2 className="normal-lg fw-medium m-0"><span className='text-info'>from</span> Thaila Mara Hall</h2>
         </Stack>
     );
