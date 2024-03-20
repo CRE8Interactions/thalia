295eeea99b392173faba5afa342e82b08e05db18
Jaime Convery <jaimeconvery@gmail.com>
2024-03-20 12:12:39 -0400

    style: edits

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -22,13 +22,13 @@ export default function Footer() {
             <Card body className='card-lg'>
                 <Stack className="d-flex-column flex-lg-row gap-lg-5 align-items-center align-items-lg-stretch">
                     <Link href="/">
-                        <Image src={logo} width="96" height="41" alt="Thalia Mara Hall logo" />
+                        <Image src={logo} width="191" height="82" alt="Thalia Mara Hall logo" className={styles.logo} />
                     </Link>
                     <Stack className={styles.info}>
-                        <h6 className='mt-3 mt-lg-0 mt-md-5 mb-1 headline-6'>Thalia Mara Hall</h6>
+                        <h6 className='mt-3 mt-lg-0 mt-md-4 mb-1 headline-6'>Thalia Mara Hall</h6>
                         <Stack gap={2} className={`gap-md-1 ${styles.address}`}>
                             <address className='m-0 text-muted fw-medium'>
-                                <p>255 East Pascagoula Street, <span className="d-block d-lg-inline">Jackson, MS 39201</span></p>
+                                <p>255 East Pascagoula Street, <span className="d-block d-md-inline">Jackson, MS 39201</span></p>
                             </address>
                             <Stack direction="horizontal" as="ul" className={styles.socialIcons}>
                                 <li>
@@ -50,7 +50,7 @@ export default function Footer() {
                         </Stack>
                     </Stack>
                     <div className={`${styles.poweredBy} d-flex flex-column flex-md-row align-items-center justify-content-center caption`}>
-                        <p className="text-muted text-center text-lg-end p-lg-4"><span>Powered by</span> <Image src={poweredBy} width="132" height="29" alt="Blocktickets logo" /></p>
+                        <p className="text-muted text-center text-lg-end px-lg-4"><span>Powered by</span> <Image src={poweredBy} width="132" height="29" alt="Blocktickets logo" /></p>
                     </div>
                 </Stack>
             </Card>
--- src/components/HomeWrapper/UpcomingEvents/EventCard/EventCard.js
+++ src/components/HomeWrapper/UpcomingEvents/EventCard/EventCard.js
@@ -27,36 +27,28 @@ export default function EventCard({ event }) {
                     alt={`Cover art for ${event?.name} event`}
                 />
                 <Card.Body>
-                    <Row className="flex-grow-1">
-                        <Col xs={2}>
-                            <Stack className='align-items-center text-center'>
-                                <span className='text-uppercase text-primary caption d-block'>{convertedStart.format('MMM')}</span>
-                                <span className="headline-4 fw-bold">{convertedStart.date()}</span>
-                            </Stack>
-                        </Col>
-                        <Col xs={10} className='d-flex-column justify-content-between'>
+                    <Stack direction="horizontal" gap={2} className="justify-content-start flex-grow-1">
+                        <div className='d-flex-column text-center h-100'>
+                            <span className='text-uppercase text-primary caption d-block'>{convertedStart.format('MMM')}</span>
+                            <span className="headline-4 fw-bold">{convertedStart.date()}</span>
+                        </div>
+                        <Stack className='justify-content-between'>
                             <div className='d-flex-column'>
                                 <Card.Title as="h5">{event.name}</Card.Title>
                                 <Card.Subtitle as="h6">{formatDateTime(moment(convertedStart))} - {formatDateTime(moment(convertedEnd), 'timeOnly')}</Card.Subtitle>
                             </div>
-                            <Row className="gap-1">
-                                <Col>
-                                    <p className='small room text-primary'>{event?.venue?.name}</p>
-                                </Col>
-                                <Row className='m-0 align-items-center'>
-                                    <Col className='ps-0'>
-                                        <span className='normal-md m-0 fw-bold'>{formatCurrency(lowTicketCost)}+</span>
-                                    </Col>
-                                    <Col className="d-flex">
-                                        <Stack className='mt-auto align-items-end'>
-                                            <span className='caption caption-bold m-0 text-nowrap'>Door time: {formatDateTime(convertedDoorsOpen, 'timeOnly')}</span>
-                                            <span className='caption text-muted m-0'>Age {event?.ageMinimum || '18'}+</span>
-                                        </Stack>
-                                    </Col>
-                                </Row>
-                            </Row>
-                        </Col>
-                    </Row>
+                            <div className="d-flex-column gap-1">
+                                <p className='small room text-primary'>{event?.venue?.name?.substring(event?.venue?.name?.indexOf(": ") + 1)}</p>
+                                <Stack direction='horizontal'>
+                                    <span className='normal-md m-0 fw-bold'>{formatCurrency(lowTicketCost)}+</span>
+                                    <Stack className='mt-auto align-items-end'>
+                                        <span className='caption caption-bold m-0 text-nowrap'>Door time: {formatDateTime(convertedDoorsOpen, 'timeOnly')}</span>
+                                        <span className='caption text-muted m-0'>Age {event?.ageMinimum || '18'}+</span>
+                                    </Stack>
+                                </Stack>
+                            </div>
+                        </Stack>
+                    </Stack>
                 </Card.Body>
             </Card>
         </Link>
